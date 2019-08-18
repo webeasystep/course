38311c51d7a37eaabb8d324b44de41a34778c8e0
Ahmed.Fakhr <fakhrawy@gmail.com>
2019-08-18 14:55:50 +0200

    add Lesson 5 to send emails

---
+++ 5-contact_us/README.md
@@ -0,0 +1,20 @@
+
+# Summary
+ * what is $_FILES as a super global and it's content
+ * add validation the form using with $_SESSION -- look to chrome
+ * where to add session_start , session_destroy and why ?
+ * file input and why should use multipart and post with files
+ * issue with handle upload files with 100000 + issue with adding date
+
+# Resources
+- Printing - variables - constants - Assignment operator
+- Concatenations - quotations (single - double)
+- Data types (integer -float - string -boolean - NULL)
+- Combine two strings in PHP
+- Write comments in PHP
+
+# Quiz
+- how to send with attachment
+- 
+
+
---
+++ 5-contact_us/sendmemail.php
@@ -0,0 +1,40 @@
+<?php session_start();
+if (isset($_POST['submit'])) {
+    $name = htmlspecialchars(stripslashes(trim($_POST['name'])));
+    $subject = htmlspecialchars(stripslashes(trim($_POST['subject'])));
+    $email = htmlspecialchars(stripslashes(trim($_POST['email'])));
+    $message = htmlspecialchars(stripslashes(trim($_POST['message'])));
+    if (!preg_match("/^[A-Za-z .'-]+$/", $name)) {
+        $name_error = 'Invalid name';
+    }
+    if (!preg_match("/^[A-Za-z .'-]+$/", $subject)) {
+        $subject_error = 'Invalid subject';
+    }
+    if (!preg_match("/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/", $email)) {
+        $email_error = 'Invalid email';
+    }
+    if (strlen($message) === 0) {
+        $message_error = 'Your message should not be empty';
+    }
+
+    $headers = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type:text/html; charset=UTF-8' . "\r\n";
+    $headers .= "From: " . $_POST['name'] . "<" . $_POST['email'] . ">\r\n";
+    $headers .= "Reply-To: " . $_POST["email"] . "\r\n";
+    $mail_subject = 'Messsage recived for' . $subject . ' Contact Page';
+    $body = $subject . '
+	
+	The person that contacted you is  ' . $name . '
+	 E-mail: ' . $email . '
+	 Subject: ' . $subject . '
+	
+	 Message: 
+	 ' . $message . '	
+	|---------END MESSAGE----------|';
+    echo "Thank you fo your feedback. I will contact you shortly if needed.<br/>
+    Go to <a href='/index.php'>Home Page</a>";
+    mail($to, $subject, $body, $headers);
+} else {
+    echo "You must write a message. </br> 
+    Please go to <a href='/index.php'>Home Page</a>";
+}
