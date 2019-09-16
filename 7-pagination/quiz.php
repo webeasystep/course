1d52cfe1539b70732886bf4bfd1e163420b7082b
Ahmed.Fakhr <fakhrawy@gmail.com>
2019-09-16 17:38:19 +0200

    update mysql lessons

---
+++ 7-pagination/quiz.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ahmed.Fakhr
+ * Date: 2019-08-27
+ * Time: 4:47 PM
+ */
+
+# Step 1 : make every page represent 10 records rather than 5
+# Step 2 : remove GENDER column from pages
+# Step 3 : add bulk action to delete multiple rows
+# Step 4 : add add active inactive option in the options column
+
