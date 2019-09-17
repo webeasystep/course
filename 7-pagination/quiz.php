877425ae2006de602ed2816a64e7ff00e6d8aa42
Ahmed.Fakhr <fakhrawy@gmail.com>
2019-09-17 09:38:51 +0200

    update mysql lessons

--- 7-pagination/quiz.php
+++ 7-pagination/quiz.php
@@ -6,13 +6,11 @@
  * Time: 4:47 PM
  */
 
-<<<<<<< HEAD
+
 # Step 1 : make every page represent 10 records rather than 5
 # Step 2 : remove GENDER column from pages
 # Step 3 : add bulk action to delete multiple rows
 # Step 4 : add add active inactive option in the options column
 
-=======
 # Step 1 : implement validation message under inputs and filter spaces with php
 # Step 2 : print every page with only 5 rows
->>>>>>> origin/master
