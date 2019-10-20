629da78e6a9b744b0040189acefc9aeba7181c14
webeasystep@gmail.com <0109953577>
2019-10-21 01:46:07 +0200

    Fix error quiz

--- 2-quote_of_the_day/lesson_1/quiz.php
+++ 2-quote_of_the_day/lesson_1/quiz.php
@@ -6,16 +6,19 @@
  * Time: 4:47 PM
  */
 
-# remove number 10 from array(1,2,3,4,5) using unset function and print it after remove
+# remove number 3 from array(1,2,3,4,5,6,7,8,9,10) using unset function and print it after remove
 // CODE HERE
 
-# loop through number 10 with Incrementing Operators (++) and print 1-2-4-5 using while , break and continue
+# loop using while to print preceding array as 1_2_4_5 using while , break and continue
+
+
+# loop using while to print numbers from 0 to 10 using while and Incrementing Operators (++)
 // CODE HERE
 
 # print array(1,2,3,4,5) as 1-2-3-4-5 using implode
 // CODE HERE
 
-# print convert 1/2/3/4/5 to array using explode
+# convert "1/2/3/4/5" string to array using explode then print it
 // CODE HERE
 
 # loop through array(1,2,3,4,5) and print as 2,3,6,8,10 using for loop
@@ -24,7 +27,7 @@
 # read line 20 on the quotes.txt file and print it
 // CODE HERE
 
-# remove line 30 on the quotes.txt file and print it
+# remove line 30 on the quotes.txt file
 // CODE HERE
 
 # add your name to the end quotes.txt file
